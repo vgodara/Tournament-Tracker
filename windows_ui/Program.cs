6ceaeaf1127fc508760d274596f3839163e9c22b
godara <viki.godara@gmail.com>
2020-11-06 10:19:28 +0530

    Added Windows Desktop UI

---
+++ windows_ui/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace windows_ui
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main ( )
+        {
+            Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault ( false );
+        }
+    }
+}
