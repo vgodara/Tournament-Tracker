5ee7e23f1bb91b4271ab1d838f71b4e5a4f1d9d4
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:11:02 +0530

    Added Base Uscase and Response class

---
+++ usecase/common/BaseUseCase.cs
@@ -0,0 +1,44 @@
+﻿using model;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace usecase.common
+{
+    abstract class BaseUseCase<Model, Request>
+    {
+        private readonly CancellationToken cancellationToken;
+        public BaseUseCase(CancellationToken cancellationToken)
+        {
+            this.cancellationToken = cancellationToken;
+        }
+        protected abstract Model BuildUseCase(Request request);
+
+        public async Task<Response<Model>> GetAsync(Request request)
+        {
+            try
+            {
+                Response<Model> response = await Task.Run<Response<Model>>(() =>
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    Model model = BuildUseCase(request);
+                    if (model == null)
+                    {
+                        throw new ArgumentNullException(nameof(Model));
+                    }
+                    else
+                        return new Response<Model>(200, $"operation was successful", model);
+                }, cancellationToken);
+                cancellationToken.ThrowIfCancellationRequested();
+                return response;
+            }
+            catch (Exception exception)
+            {
+                return new Response<Model>(exception.InnerException.HResult, exception.Message, exception);
+            }
+
+
+
+        }
+    }
+}
