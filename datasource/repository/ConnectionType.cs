6aa5fd84bd7079feeae93eb9e451d82eb2fd3199
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:45:29 +0530

    Added Connection Type Enum

---
+++ datasource/repository/ConnectionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datasource.repository
+{
+   public enum ConnectionType
+    {
+        SQL,
+        LOCAL
+    }
+}
