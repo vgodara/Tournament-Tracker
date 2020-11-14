4ceef75204f31d2c2e84a93454bdae1acfdef0c2
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:07:32 +0530

    Added Repository Interface

---
+++ usecase/IRepository.cs
@@ -0,0 +1,40 @@
+﻿using model;
+using LanguageExt;
+
+namespace usecase
+{
+   public  interface IRepository
+    {
+        Lst<PersonModel> GetAllPersons();
+        PersonModel GetPerson(int id);
+        PersonModel SavePerson(PersonModel person);
+
+
+        Lst<TeamModel> GetAllTeams();
+        TeamModel GetTeam(int id);
+        TeamModel SaveTeam(TeamModel team);
+
+
+        Lst<PrizeModel> GetAllPrizes();
+        PrizeModel GetPrize(int id);
+        PrizeModel SavePrize(PrizeModel prize);
+
+        Lst<MatchUpEntryModel> GetAllMatchUpEntries();
+        MatchUpEntryModel GetMatchUpEntry(int id);
+        MatchUpEntryModel SaveMatchUpEntry(MatchUpEntryModel matchUpEntry);
+
+
+        Lst<MatchUpModel> GetAllMatchUps();
+        MatchUpModel GetMatchUp(int id);
+        MatchUpModel SaveMatchUp(MatchUpModel matchUp);
+
+        Lst<RoundModel> GetAllRounds();
+        RoundModel GetRound(int id);
+        RoundModel SaveRound(RoundModel round);
+
+
+        Lst<TournamentModel> GetAllTournaments();
+        TournamentModel GetTournament(int id);
+        TournamentModel SaveTournament(TournamentModel tournament);
+    }
+}
