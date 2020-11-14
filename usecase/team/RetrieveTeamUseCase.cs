566db2b1a3950b59032bef1da969a5d68bc5e547
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:12:57 +0530

    Added Team Related Usecases

---
+++ usecase/team/RetrieveTeamUseCase.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using model;
+using usecase.common;
+namespace usecase.team
+{
+    class RetrieveTeamUseCase : BaseUseCase<TeamModel, int>
+    {
+        private readonly IRepository repository;
+
+        public RetrieveTeamUseCase(IRepository repository, CancellationToken cancellationToken) : base(cancellationToken) => this.repository = repository;
+
+        protected override TeamModel BuildUseCase(int request)
+        {
+            return repository.GetTeam(request);
+        }
+    }
+}
---
+++ usecase/team/SaveTeamUseCase.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using model;
+using usecase.common;
+
+namespace usecase.team
+{
+    class SaveTeamUseCase : BaseUseCase<TeamModel, TeamModel>
+    {
+        private readonly IRepository repository;
+
+        public SaveTeamUseCase(IRepository repository, CancellationToken cancellationToken) : base(cancellationToken) => this.repository = repository;
+
+        protected override TeamModel BuildUseCase(TeamModel request)
+        {
+            return repository.SaveTeam(request);
+        }
+    }
+}
