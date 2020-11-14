7e21b21057a3994c7692a3fc8a79a34853c25220
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:23:42 +0530

    Added Tournament Related Usecases

---
+++ usecase/tournament/RetriveAllTournamentUseCase.cs
@@ -0,0 +1,20 @@
+﻿using LanguageExt;
+using model;
+using System.Threading;
+using usecase.common;
+
+
+namespace usecase.round
+{
+    class RetriveAllTournamentUseCase : BaseUseCase<Lst<TournamentModel>, object>
+    {
+        private readonly IRepository repository;
+
+        public RetriveAllTournamentUseCase(IRepository repository, CancellationToken cancellationToken) : base(cancellationToken) => this.repository = repository;
+
+        protected override Lst<TournamentModel> BuildUseCase(object request)
+        {
+            return repository.GetAllTournaments();
+        }
+    }
+}
