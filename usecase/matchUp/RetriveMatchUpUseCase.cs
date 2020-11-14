70239b006d730fdd503a7010fa6b43795a6eb4dc
Vikas Godara <viki.godara@gmail.com>
2020-11-15 03:21:39 +0530

    Added Match Up Related Usecases

---
+++ usecase/matchUp/RetriveMatchUpUseCase.cs
@@ -0,0 +1,18 @@
+﻿using model;
+using usecase.common;
+using System.Threading;
+
+namespace usecase.matchUp
+{
+    class RetriveMatchUpUseCase : BaseUseCase<MatchUpModel,int>
+    {
+        private readonly IRepository repository;
+
+        public RetriveMatchUpUseCase(IRepository repository, CancellationToken cancellationToken) : base(cancellationToken) => this.repository = repository;
+
+        protected override MatchUpModel BuildUseCase(int request)
+        {
+            return repository.GetMatchUp(request);
+        }
+    }
+}
---
+++ usecase/matchUp/SaveMatchUpUseCase.cs
@@ -0,0 +1,18 @@
+﻿using model;
+using usecase.common;
+using System.Threading;
+
+namespace usecase.matchUp
+{
+    class SaveMatchUpUseCase : BaseUseCase<MatchUpModel, MatchUpModel>
+    {
+        private readonly IRepository repository;
+
+        public SaveMatchUpUseCase(IRepository repository, CancellationToken cancellationToken) : base(cancellationToken) => this.repository = repository;
+
+        protected override MatchUpModel BuildUseCase(MatchUpModel request)
+        {
+            return repository.SaveMatchUp(request);
+        }
+    }
+}
