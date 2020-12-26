b9b209c324a05c828880d1e373c2f97199d90558
Vikas Godara <viki.godara@gmail.com>
2020-12-26 18:00:58 +0530

    Pivot table returns model now

--- SQLDataBase/Store Procedures/TeamMembers/spTeamMember_saveTeamMember.sql
+++ SQLDataBase/Store Procedures/TeamMembers/spTeamMember_saveTeamMember.sql
@@ -1,12 +1,18 @@
 ﻿CREATE PROCEDURE spTeamMember_saveTeamMember
 	@TeamId INT,
-	@TeamMemberJson NVARCHAR(MAX)
+	@InputTeamMemberJson NVARCHAR(MAX),
+	@OutputTeamMemberJson NVARCHAR(MAX) OUTPUT  
 AS
 BEGIN
-	IF(ISJSON(@TeamMemberJson)!=1)
+	IF(ISJSON(@InputTeamMemberJson)!=1)
 		THROW 400, N'Invalid Json', 1
-	DECLARE @PersonId INT 
+	DECLARE @InputPersonId INT, 
+			@OutputPersonId INT,
+			@TeamMemberId INT
 
-	SET @PersonId = JSON_VALUE(@TeamMemberJson,'$.Id')
-	INSERT INTO TeamMembers(TeamId,PersonId) VALUES (@TeamId,@PersonId)
+	SET @InputPersonId = JSON_VALUE(@InputTeamMemberJson,'$.Id')
+	INSERT INTO TeamMembers(TeamId,PersonId) VALUES (@TeamId,@InputPersonId)
+	SET @TeamMemberId = SCOPE_IDENTITY()
+	SET @OutputPersonId = (SELECT PersonId FROM TeamMembers WHERE Id = @TeamMemberId)
+	EXEC spPeopel_GetPersonById @PersonId = @OutputPersonId,@OutputJson = @OutputTeamMemberJson
 END 
--- SQLDataBase/Store Procedures/TeamMembers/spTeamMember_saveTeamMembers.sql
+++ SQLDataBase/Store Procedures/TeamMembers/spTeamMember_saveTeamMembers.sql
@@ -1,23 +1,27 @@
 ﻿CREATE PROCEDURE spTeamMember_saveTeamMembers
-		@TeamJson NVARCHAR(MAX)
+		@TeamId INT,
+		@InputTeamMembersJson NVARCHAR(MAX),
+		@OutputTeamMembersJson NVARCHAR(MAX)
 	
 AS
 BEGIN
 	SET NOCOUNT ON 
-	IF(ISJSON(@TeamJson)!=1)
+	IF(ISJSON(@InputTeamMembersJson)!=1)
 		THROW 400, N'Invalid Json', 1
-	DECLARE @TeamId INT,
-			@CurrentTeamMemberIndex INT,
-			@TotalTeamMembers INT
+	DECLARE @CurrentTeamMemberIndex INT=0,
+			@TotalTeamMembers INT = (SELECT COUNT(*) FROM OPENJSON(@InputTeamMembersJson, '$'))
 
-	SET @TeamId = JSON_VALUE(@TeamJson,'$.Id')
-	SET @CurrentTeamMemberIndex = 0
-	SET @TotalTeamMembers = (SELECT COUNT(*) FROM OPENJSON(@TeamJson, '$.TeamMembers'))
+	SET @OutputTeamMembersJson = N'[]'
 	WHILE (@CurrentTeamMemberIndex < @TotalTeamMembers)
 	BEGIN
-		DECLARE @TeamMemberJson NVARCHAR(MAX) 
-		SET @TeamMemberJson = JSON_QUERY(@TeamJson, '$.TeamMembers['+CAST(@CurrentTeamMemberIndex AS VARCHAR)+']' )
-		EXEC spTeamMember_saveTeamMember @TeamId = @TeamId, @TeamMemberJson = @TeamMemberJson 
+		DECLARE @InputTeamMemberJson NVARCHAR(MAX), 
+				@OutputTeamMemberJson NVARCHAR(MAX)
+				
+		SET @InputTeamMemberJson = JSON_QUERY(@InputTeamMembersJson, '$['+CAST(@CurrentTeamMemberIndex AS VARCHAR)+']' )
+		EXEC spTeamMember_saveTeamMember @TeamId = @TeamId, 
+										 @InputTeamMemberJson = @InputTeamMemberJson, 
+										@OutputTeamMemberJson = @OutputTeamMemberJson
+		SET @OutputTeamMembersJson = JSON_MODIFY(@OutputTeamMembersJson, 'append $' ,@OutputTeamMemberJson)
 		SET @CurrentTeamMemberIndex = @CurrentTeamMemberIndex+1
 	END
 END
